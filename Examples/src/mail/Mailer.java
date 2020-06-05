f483c1a981c9829665a11d3bfb6a7dc3506cc958
admin <admin@DESKTOP-H6SUMKT>
2020-06-05 17:47:07 +0900

    06-05-

--- Examples/src/mail/Mailer.java
+++ Examples/src/mail/Mailer.java
@@ -8,11 +8,12 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-
 public class Mailer {
+
 	public void sendMail(String to, String subject, String content, SMTPAuthenticator smtp) {
 // SMTP 서버 정보 저장
 		Properties p = new Properties();
+
 		p.put("mail.smtp.host", "smtp.gmail.com");
 		p.put("mail.smtp.port", "465");
 		p.put("mail.smtp.starttls.enable", "true");
@@ -26,8 +27,9 @@ public class Mailer {
 			ses.setDebug(true);
 			MimeMessage msg = new MimeMessage(ses); // 메일의 내용을 담을 객체
 			msg.setSubject(subject); // 제목
-			Address fromAddr = new InternetAddress("yh9407@gmail.com");
-			msg.setFrom(fromAddr); // 보내는 사람
+			// Address fromAddr = new InternetAddress("yh9407@gmail.com");
+			InternetAddress adress = new InternetAddress("yh9407@gmail.com", "한글도되나");
+			msg.setFrom(adress); // 보내는 사람
 			Address toAddr = new InternetAddress(to);
 			msg.addRecipient(Message.RecipientType.TO, toAddr); // 받는 사람
 			msg.setContent(content, "text/html;charset=UTF-8"); // 내용과 인코딩
